c53b7e0187d7e467068a1df2fc680f47ef6c3821
Des G Bauza <dgb.javadeveloper@gmail.com>
2023-11-23 00:07:12 -0500

    Add color to sheet

--- Documents/OIT/scarlettSafetyApp/src/Server.js
+++ Documents/OIT/scarlettSafetyApp/src/Server.js
@@ -1,3 +1,7 @@
+
+// S *** Q: How does javascript execution of code run
+ // all things declared 
+ // and then middle space where other code can be called?
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 5500;
@@ -13,14 +17,23 @@ const server = http.createServer((req, res) => {
   function myFunction() {
     window.open("tel:+15678675309");
   }
+  
+  // Option 1 
+  console.log(new Date().Time());
+
   </script>
 </html>`);
 });
+
+// Option 2
+console.log(new Date().getDate());
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-  console.log(new Date().getTime());
+  /* console.log(`Server running at http://${hostname}:${port}/`);
+  
+  // Option 3 
+  console.log(new Date().getHour());
   console.log(new Date().getDate());
   console.log("Lattitude"+ navigator.geolocation.getCurrentPosition.coords());
   console.log("Longitude"+ navigator.geolocation.getCurrentPosition.coords());
-
+    */
 });
