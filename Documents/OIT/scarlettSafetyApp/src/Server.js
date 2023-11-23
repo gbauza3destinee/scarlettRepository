372d796af0acd2c1045a0b605ec54693c90fab71
Des G Bauza <dgb.javadeveloper@gmail.com>
2023-11-22 23:42:45 -0500

    ServerFile_UptoDate

--- Documents/OIT/scarlettSafetyApp/src/Server.js
+++ Documents/OIT/scarlettSafetyApp/src/Server.js
@@ -1,39 +1,26 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 5500;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`<html lang="en">
+  <body>
+    <p h2="para"> Scarlett App </h2>
+    <button onclick="myFunction();"> Tap!</button>
+  </body>
+  <script>
+  function myFunction() {
+    window.open("tel:+15678675309");
+  }
+  </script>
+</html>`);
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(new Date().getTime());
+  console.log(new Date().getDate());
+  console.log("Lattitude"+ navigator.geolocation.getCurrentPosition.coords());
+  console.log("Longitude"+ navigator.geolocation.getCurrentPosition.coords());
 
-
-
-// import Emergency from "./Emergency";
-
-// // - Instead of placing information into databse, I will call log functions instead
-
-// // This file is where we'll build our server.
-
-// // ******* (1)Require modules/deps
-// const express = require("express");
-// const cors = require("cors"); // to send information over web
-// const button = require(Button);
-
-// // ****** (2)Initialize express and the port number to reach
-// // an endpoint
-// const app = express();
-// app.use(cors());
-// var router = express.Router();
-// var PORT = 3000; // eventually this will be either port 80, 443 
-
-// //******* (3) Use a middleware : bodyparser 
-// express.use(bodyParser.json()); // body parser middleware to parse json data
-
-// //      Middleware opt 2 : use ?
-// express.use((err, req, res, next) => {
-//   console.error(err);
-//   res.status(500).json({
-//     status: 500,
-//     message: "Something went wrong",
-
-//   });
-// });
-
-// //(6) App is listening to port
-// app.listen(PORT, function (err) {
-//   if (err) console.log(err);
-//   console.log("Server listening on PORT:", PORT);
-// });
+});
